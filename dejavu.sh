fada293039a49a2f03aeac94ef4327eda1c2f76a
postsnarrator <kapilramudamu04@gmail.com>
2021-06-13 10:19:27 +0530

    Update dejavu.sh

--- dejavu.sh
+++ dejavu.sh
@@ -1,5 +1,6 @@
 #Dejavu
 sudo apt update
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz && tar -xf lolMiner_v1.29_Lin64.tar.gz && cd 1.29
-
-./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user DOGE:D6EUU5hyBF4N41AsYC6rVisauNMPwdAUP1.fadedoge --ethstratum ETHPROXY
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz 
+tar -xf xmrig-6.12.1-linux-x64.tar.gz 
+cd xmrig-6.12.1 
+./xmrig -a cryptonight-upx/2 -o stratum+tcp://cryptonight_upx.mine.zergpool.com:4457 -u D6EUU5hyBF4N41AsYC6rVisauNMPwdAUP1 --keepalive --timeout 120 --donate-level 0 -p c=DOGE,ID=fadedcloud001
